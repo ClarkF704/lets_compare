0925f095fffd1cff3b458d12bd61cf966e9c4674
Clark Farriss <clarkfarriss@Clarks-MacBook-Pro.local>
2019-06-01 18:35:34 -0400

    changed react HTTP post

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,47 +1,38 @@
 import React, { Component } from 'react';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
 import axios from 'axios';
-import { request } from 'https';
 import Post from '../Post/Post';
 
 
 
 
 class Landing extends Component {
-    constructor(){
-      super()
-  
-      this.state = {
-        name: '',
-        list: [],
-        error: null
-      }
-      this.handleChange = this.handleChange.bind(this)
-      this.handleSubmit = this.handleSubmit.bind(this)
-    }
-    
-   
-      
-  
-    handleChange = e => {
-      this.setState({[e.target.name]: e.target.value})
-    }
-    async handleSubmit(e) {
-      e.preventDefault()
-  
-      const {name} = this.state
-  
-      const form = await axios.post('/api/form', {
-        name
-      });
+  constructor(props){
+    super(props)
 
+    this.state = {
+      name: ''
     }
+  }
+  
+  changeHandler = (e) =>{
+    this.setState({[e.target.name]: e.target.value})
+  }
     
+  submitHandler = (e) =>{
+    e.preventDefault()
+    console.log(this.state)
+    axios.post("/api/form", this.state)
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error =>{
+      console.log(error)
+    })
+  }
    
   
     render() {
-        
+        const {name} = this.state
       return (
         <div>
           <div className="container">
@@ -49,17 +40,18 @@ class Landing extends Component {
           <div className="col-lg-12">
             <h1 style={{textAlign:'center',fontFamily:'Roboto Mono', color:'white'}}>Let's Compare</h1>
             <p style={{textAlign:'center',fontFamily:'Roboto Mono', color:'white'}}><span style={{fontStyle:'italic'}}>Let's Compare </span>is a scraper tool that scrapes the top 5 items from Bing.com and Craigslist Charlotte.</p>
-          <form onSubmit={this.handleSubmit} style={{textAlign:'center'}}>
+          <form onSubmit={this.submitHandler} style={{textAlign:'center'}}>
             <input 
                 label="name"
                 margin="normal"
                 variant="outlined"
                 name="name"
-                for="name"
+                value={name}
+                onChange={this.changeHandler}
                 style={{fontFamily:'Roboto Mono'}}
-                onChange={this.handleChange}
+                
             />
-            <button variant="contained" style={{fontFamily:'Roboto Mono'}}>Compare!</button>
+            <button variant="contained" style={{fontFamily:'Roboto Mono'}} type="submit">Compare!</button>
             </form>
           </div>
           </div>
